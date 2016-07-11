b43b991b9a74d1aa9d155f9a221350c3d640c2c2
Mike Thal <mikeathal@gmail.com>
2016-07-11 16:04:20 -0400

    Pushing First text files including TapGame

---
+++ Assets/Scripts/TapGame.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class TapGame : MonoBehaviour {
+
+	// declare a Text variable so that this code can talk to it
+	public Text myTextThing;
+
+	int currentPoints = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+		myTextThing.text = "Current Score:" + currentPoints.ToString ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// give player 1 point if they press Space
+		if ( Input.GetKeyDown(KeyCode.Space) ) {
+			currentPoints += 1;
+			myTextThing.text = "Current Score: " + currentPoints.ToString();
+		}
+
+		if (Input.GetKeyDown (KeyCode.X)) {
+			currentPoints += 1000;
+		}
+
+		if (currentPoints >= 100) {
+			myTextThing.text = "Winner Winner, Chicken Dinner!";
+		}
+
+	}
+}
