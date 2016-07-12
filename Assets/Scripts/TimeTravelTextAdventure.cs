25dcac3c3294f8e95aedceda874461ff0c70ae9c
Mike Thal <mikeathal@gmail.com>
2016-07-12 13:16:32 -0400

    Update with code that connects lobby to elevators and outside

--- Assets/Scripts/TimeTravelTextAdventure.cs
+++ Assets/Scripts/TimeTravelTextAdventure.cs
@@ -19,11 +19,23 @@ string currentRoom = "Lobby"; // remembers our current location in world
 		string textBuffer = "You are currently in: " + currentRoom;
 
 		if ( currentRoom == "Lobby" ) {
-		// all of your LOBBY code will go here later!
+			// typing "\n" in a string means "line break"
+			textBuffer += "\nYou see the security guard.";
+			// the line of code below is the SAME as the line of code above
+			// textBuffer = textBuffer + "\nYou see the security guard.";
+
+			textBuffer += "\nPress [W] to go to elevators";
+			textBuffer += "\nPress [S] to go outside";
+
+			if (Input.GetKeyDown (KeyCode.W)) { // if player pushes W...
+				currentRoom = "Elevators";
+			} else if (Input.GetKeyDown (KeyCode.S)) {
+				currentRoom = "Outside";
+			}
 		} else if ( currentRoom == "Elevators" ) {
-		// all of your ELEVATORS code will go here later!
+			// all of your ELEVATORS code will go here later!
 		} else if ( currentRoom == "Outside" ) {
-		// all of your OUTSIDE code will go here later!
+			// all of your OUTSIDE code will go here later!
 		}
 		// let's "render out" our text buffer and display it on-screen
 		// we need to do this at the end, otherwise we'll display text too soon
