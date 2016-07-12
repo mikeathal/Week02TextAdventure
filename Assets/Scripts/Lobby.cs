479a8ebc8f5efd470ecca5a604899f1d7c4bfde5
Mike Thal <mikeathal@gmail.com>
2016-07-12 12:53:58 -0400

    Pushing the script from Section 2 of the Lab assignment

--- Assets/Scripts/Lobby.cs
+++ Assets/Scripts/Lobby.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Lobby : MonoBehaviour {
+using UnityEngine.UI;
+
+string currentRoom = "Lobby"; // remembers our current location in world
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
-	}
-}
+		// if I declare a variable inside Update(),
+		// then I can ONLY use this variable inside Update() !!
+		// also, a "buffer" is a staging area to prepare data
+		string textBuffer = "You are currently in: " + currentRoom;
+
+		if ( currentRoom == "Lobby" ) {
+		// all of your LOBBY code will go here later!
+		} else if ( currentRoom == "Elevators" ) {
+		// all of your ELEVATORS code will go here later!
+		} else if ( currentRoom == "Outside" ) {
+		// all of your OUTSIDE code will go here later!
+		}
+		// let's "render out" our text buffer and display it on-screen
+		// we need to do this at the end, otherwise we'll display text too soon
+		GetComponent<Text>().text = textBuffer;
+	}// CLOSES OUT UPDATE() scope
+
