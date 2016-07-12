c75ad596ef3d7a178998dea2aad8f70f0a7e9230
Mike Thal <mikeathal@gmail.com>
2016-07-12 14:41:35 -0400

    Updated files with completed version from worksheet

--- Assets/Scripts/TimeTravelTextAdventure.cs
+++ Assets/Scripts/TimeTravelTextAdventure.cs
@@ -8,19 +8,11 @@ public class TimeTravelTextAdventure : MonoBehaviour {
 	string currentRoom = "Lobby"; // remembers our current location in world
 
 		bool hasStudentID = false;
-
 		void Update () {
-		// if I declare a variable inside Update(),
-		// then I can ONLY use this variable inside Update() !!
-		// also, a "buffer" is a staging area to prepare data
 		string textBuffer = "You are currently in: " + currentRoom;
 
 		if (currentRoom == "Lobby") {
-			// typing "\n" in a string means "line break"
 			textBuffer += "\nYou see the security guard.";
-			// the line of code below is the SAME as the line of code above
-			// textBuffer = textBuffer + "\nYou see the security guard.";
-
 			textBuffer += "\nPress [W] to go to elevators";
 			textBuffer += "\nPress [S] to go outside";
 
@@ -31,14 +23,20 @@ public class TimeTravelTextAdventure : MonoBehaviour {
 			}
 		} else if (currentRoom == "Elevators") {
 			textBuffer += "\nYou're waiting.";
-			// But which room will let the user set "hastStudentID" to true?
 			if (hasStudentID == false) {
 				textBuffer += "\nYou can't go in without your ID card, though...";
 			} else {
 				textBuffer += "\nYou swipe your Student ID and the guard smiles.";
 			}
-		} else if ( currentRoom == "Outside" ) {
-			// all of your OUTSIDE code will go here later!
+		} else if (currentRoom == "Outside") {
+			textBuffer += "\nIT IS REALLY HOT WHAT IS WRONG WITH YOU";
+			textBuffer += "\nPress [S] to go back INSIDE, LIKE RIGHT NOW";
+			textBuffer += "\n(Oh hey you found your student ID on the floor!)";
+			hasStudentID = true;
+
+			// when "if" statement has no curly braces, then only the next line happens
+			if (Input.GetKeyDown (KeyCode.S))
+				currentRoom = "Lobby";
 		}
 		// let's "render out" our text buffer and display it on-screen
 		// we need to do this at the end, otherwise we'll display text too soon
