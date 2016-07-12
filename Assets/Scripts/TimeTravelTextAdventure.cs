244a41b3c4714da299847a87ebc2a7f178da778d
Mike Thal <mikeathal@gmail.com>
2016-07-12 16:14:50 -0400

    Added 2 new rooms. Renaissance room has first key. Earth 2107 A.D. Room still doesn't work yet.

--- Assets/Scripts/TimeTravelTextAdventure.cs
+++ Assets/Scripts/TimeTravelTextAdventure.cs
@@ -26,36 +26,60 @@ public class TimeTravelTextAdventure : MonoBehaviour {
 			} else if (Input.GetKeyDown (KeyCode.A)) {
 				currentRoom = "Atlantis";
 			} else if (Input.GetKeyDown (KeyCode.R)) {
-				currentRoom = "Rennaisance";
+				currentRoom = "The Rennaisance";
 			} else if (Input.GetKeyDown (KeyCode.E)) {
-				currentRoom = "Earth";
+				currentRoom = "Earth 2107 A.D.";
 			} else if (Input.GetKeyDown (KeyCode.N)) {
 				currentRoom = "Nebulo 9";
 			}
 
 		} else if (currentRoom == "Jurassic Era") {
-				textBuffer += "\nJurassic Era it is!";
-				textBuffer += "\n\nYou try to walk out of your time machine and a T-Rex is staring you in the face.";
-				textBuffer += "\nMaybe this wasn't a good idea...";
-				textBuffer += "\n\nPress [C] to go back to the Control Room.";
-				if (Input.GetKeyDown (KeyCode.C))
-					currentRoom = "Control Room";
+			textBuffer += "\n\nJurassic Era it is!";
+			textBuffer += "\n\nYou walk out of your time machine and a T-Rex is staring you in the face.";
+			textBuffer += "\nMaybe this wasn't a good idea...";
+			textBuffer += "\n\nPress [C] to go back to the Control Room.";
+			if (Input.GetKeyDown (KeyCode.C))
+				currentRoom = "Control Room";
 
-			} else if (currentRoom == "Atlantis") {
-				textBuffer += "\n\nTo Atlantis then! Don't forget your bathingsuit!";
+		} else if (currentRoom == "Atlantis") {
+			textBuffer += "\n\nTo Atlantis then! Don't forget your bathingsuit!";
 
-				if (hasKingsPendant == false) {
-					textBuffer += "\n\nThe gates are locked.  There seems to be a pendant shaped hole in the door. ";
-					textBuffer += "What a strange looking mechanism...";
-				} else {
-					textBuffer += "\nThe King's Pendant you found fits perfectly! The gates open and you are greeted with open arms.";
-					textBuffer += "\nKing Raigen shows all around the city.  There are fish with colors the likes you've never seen!"; 
-					textBuffer += "Fortunately all of the sharks are domesticated too.";
-				}
-				textBuffer += "\n\nPress [C] to go back to the Control Room.";
-				if (Input.GetKeyDown (KeyCode.C))
-					currentRoom = "Control Room";
+			if (hasKingsPendant == false) {
+				textBuffer += "\n\nThe gates are locked.  There seems to be a pendant shaped hole in the door. ";
+				textBuffer += "What a strange looking mechanism...";
+			} else {
+				textBuffer += "\n\nThe King's Pendant you found fits perfectly! The gates open and you are greeted with open arms. ";
+				textBuffer += "King Raigen shows all around the city.  There are fish with colors the likes of which you've never seen! "; 
+				textBuffer += "Fortunately all of the sharks are domesticated too.";
 			}
-		GetComponent<Text>().text = textBuffer;
+			textBuffer += "\n\nPress [C] to go back to the Control Room.";
+			if (Input.GetKeyDown (KeyCode.C))
+				currentRoom = "Control Room";
+			
+		} else if (currentRoom == "The Rennaisance") {
+			textBuffer += "\n\nOff to The Rennaisance, Lords and Ladies!";
+			textBuffer += "\n\nYou exit your time machine right in the middle of a market.";
+			textBuffer += "\nThere's a man selling coconuts that he claims to have found, and another man with a cart yelling 'BRING OUT YER DEAD!'";
+			textBuffer += "\n\nAmidst the crowded streets, you find an 'A' shaped pendant on the ground. It looks shiny so you decide to take it!";
+			hasKingsPendant = true;
+
+			textBuffer += "\n\nPress [C] to go back to the Control Room.";
+			if (Input.GetKeyDown (KeyCode.C))
+				currentRoom = "Control Room";
+
+		} else if (currentRoom = "Earth 2107 A.D.") {
+			textBuffer += "\n\nUgh... I SUPPOSE you can go to Earth.";
+			textBuffer += "\n\nThe moment you step outside, see lazers are flying everywhere across a desolate wasteland. ";
+			textBuffer += "The people of Earth are at war with each other again.  It seems like not much as changed in 100 years.";
+
+			textBuffer += "\n\nOn the bright side, you find a glowing fuel box that could probably fit in your Hyperdrive Engine!";
+			hasHyperDrive = true;
+
+			textBuffer += "\n\nPress [C] to go back to the Control Room.";
+			if (Input.GetKeyDown (KeyCode.C))
+				currentRoom = "Control Room";
+
 		}
-	}// CLOSES OUT UPDATE() scope
+		GetComponent<Text>().text = textBuffer;
+	}
+}// CLOSES OUT UPDATE() scope
