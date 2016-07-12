53612e56f60b0b5d6deb97532429ca2e0f275fbe
Mike Thal <mikeathal@gmail.com>
2016-07-12 12:43:57 -0400

    Renamed script to "Lobby" to match Lab worksheet

--- Assets/Scripts/Lobby.cs
+++ Assets/Scripts/Lobby.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TimeTravelTextAdventure : MonoBehaviour {
+public class Lobby : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
