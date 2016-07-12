90cba8a4fa849cf55ebf3279953eb018fbd22667
Mike Thal <mikeathal@gmail.com>
2016-07-12 15:47:49 -0400

    Updates with first three rooms of Text Adventure and first Gate

--- Assets/Scripts/TimeTravelTextAdventure.cs
+++ Assets/Scripts/TimeTravelTextAdventure.cs
@@ -5,41 +5,57 @@ using UnityEngine.UI;
 
 public class TimeTravelTextAdventure : MonoBehaviour {
 	
-	string currentRoom = "Lobby"; // remembers our current location in world
+	string currentRoom = "Control Room"; // remembers our current location in world
 
-		bool hasStudentID = false;
-		void Update () {
-		string textBuffer = "You are currently in: " + currentRoom;
+		bool hasKingsPendant = false;
+		bool hasHyperDrive = false;
+	void Update () {
+		string textBuffer = "You are in: " + currentRoom;
 
-		if (currentRoom == "Lobby") {
-			textBuffer += "\nYou see the security guard.";
-			textBuffer += "\nPress [W] to go to elevators";
-			textBuffer += "\nPress [S] to go outside";
+		if (currentRoom == "Control Room") {
+			textBuffer += "\n\nYou are a time traveler, exploring the realms of time and space for fun.";
+			textBuffer += "\n\nWhen would you like to go?";
+			textBuffer += "\nPress [J] to go to the Jurassic Era";
+			textBuffer += "\nPress [A] to go to the Ancient City of Atlantis";
+			textBuffer += "\nPress [R] to go to the Rennaisance";
+			textBuffer += "\nPress [E] to go to Earth 2107 A.D.";
+			textBuffer += "\nPress [N] to go to the Nebulo 9";
 
-			if (Input.GetKeyDown (KeyCode.W)) { // if player pushes W...
-				currentRoom = "Elevators";
-			} else if (Input.GetKeyDown (KeyCode.S)) {
-				currentRoom = "Outside";
+			if (Input.GetKeyDown (KeyCode.J)) { // if player pushes J...
+				currentRoom = "Jurassic Era";
+			} else if (Input.GetKeyDown (KeyCode.A)) {
+				currentRoom = "Atlantis";
+			} else if (Input.GetKeyDown (KeyCode.R)) {
+				currentRoom = "Rennaisance";
+			} else if (Input.GetKeyDown (KeyCode.E)) {
+				currentRoom = "Earth";
+			} else if (Input.GetKeyDown (KeyCode.N)) {
+				currentRoom = "Nebulo 9";
 			}
-		} else if (currentRoom == "Elevators") {
-			textBuffer += "\nYou're waiting.";
-			if (hasStudentID == false) {
-				textBuffer += "\nYou can't go in without your ID card, though...";
-			} else {
-				textBuffer += "\nYou swipe your Student ID and the guard smiles.";
-			}
-		} else if (currentRoom == "Outside") {
-			textBuffer += "\nIT IS REALLY HOT WHAT IS WRONG WITH YOU";
-			textBuffer += "\nPress [S] to go back INSIDE, LIKE RIGHT NOW";
-			textBuffer += "\n(Oh hey you found your student ID on the floor!)";
-			hasStudentID = true;
 
-			// when "if" statement has no curly braces, then only the next line happens
-			if (Input.GetKeyDown (KeyCode.S))
-				currentRoom = "Lobby";
-		}
-		// let's "render out" our text buffer and display it on-screen
-		// we need to do this at the end, otherwise we'll display text too soon
+		} else if (currentRoom == "Jurassic Era") {
+				textBuffer += "\nJurassic Era it is!";
+				textBuffer += "\n\nYou try to walk out of your time machine and a T-Rex is staring you in the face.";
+				textBuffer += "\nMaybe this wasn't a good idea...";
+				textBuffer += "\n\nPress [C] to go back to the Control Room.";
+				if (Input.GetKeyDown (KeyCode.C))
+					currentRoom = "Control Room";
+
+			} else if (currentRoom == "Atlantis") {
+				textBuffer += "\n\nTo Atlantis then! Don't forget your bathingsuit!";
+
+				if (hasKingsPendant == false) {
+					textBuffer += "\n\nThe gates are locked.  There seems to be a pendant shaped hole in the door. ";
+					textBuffer += "What a strange looking mechanism...";
+				} else {
+					textBuffer += "\nThe King's Pendant you found fits perfectly! The gates open and you are greeted with open arms.";
+					textBuffer += "\nKing Raigen shows all around the city.  There are fish with colors the likes you've never seen!"; 
+					textBuffer += "Fortunately all of the sharks are domesticated too.";
+				}
+				textBuffer += "\n\nPress [C] to go back to the Control Room.";
+				if (Input.GetKeyDown (KeyCode.C))
+					currentRoom = "Control Room";
+			}
 		GetComponent<Text>().text = textBuffer;
+		}
 	}// CLOSES OUT UPDATE() scope
-}
